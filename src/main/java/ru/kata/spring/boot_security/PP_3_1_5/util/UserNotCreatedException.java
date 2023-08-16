2f5c76e6bd4e58452fed541c4f681103ee38bdbd
Maxim Belousov <124480438+Maxibelo@users.noreply.github.com>
2023-08-16 21:31:27 +0300

    Delete src/main/java/ru/kata/spring/boot_security/PP_3_1_5/util directory

--- src/main/java/ru/kata/spring/boot_security/PP_3_1_5/util/UserNotCreatedException.java
+++
@@ -1,8 +0,0 @@
-package ru.kata.spring.boot_security.PP_3_1_5.util;
-
-public class UserNotCreatedException extends RuntimeException {
-
-    public UserNotCreatedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/kata/spring/boot_security/PP_3_1_5/util/UserNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package ru.kata.spring.boot_security.PP_3_1_5.util;
-
-public class UserNotFoundException  extends RuntimeException{
-}
